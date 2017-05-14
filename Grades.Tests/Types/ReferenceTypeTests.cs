bbfb2150f5e67bc43e721fcdfa31692fd45d322c
Samuel Bancroft <samuelbancroft@hotmail.com>
2017-05-14 15:19:00 +0100

    fixed small syntactic error.

--- Grades.Tests/Types/ReferenceTypeTests.cs
+++ Grades.Tests/Types/ReferenceTypeTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AITDNTwo;
+﻿using AITDNTwo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Grades.Tests.Types
